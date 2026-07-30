ee4b4368c50c4e99a9b6ba874a2656aa3e710587
abrigo <chistiakov.dn@dom.ru>
2026-07-30 14:11:32 +0500

    add admin models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "author", "category", "created_at", "updated_at")
+    list_filter = ("category", "created_at", "author")
+    search_fields = ("title", "content", "author__username")
+    readonly_fields = ("created_at", "updated_at")
+
+    raw_id_fields = ("author",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ("post", "author", "content_short", "created_at")
+    list_filter = ("created_at", "author")
+    search_fields = ("content", "post__title", "author__username")
+    readonly_fields = ("created_at",)
+    raw_id_fields = ("post", "author")
+
+    def content_short(self, obj):
+        return obj.content[:50] + "..." if len(obj.content) > 50 else obj.content
+
+    content_short.short_description = "Текст (сокращенно)"
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+
+    list_display = ("username", "email", "bio", "is_staff", "is_active", "date_joined")
+
+    list_filter = ("is_staff", "is_active", "date_joined")
+
+    search_fields = ("username", "email", "bio")
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Дополнительная информация", {"fields": ("bio",)}),
+    )
