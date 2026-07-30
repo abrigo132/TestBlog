86ccfc7e096e43ffb9fbe0fe8a673e599ae5b80c
abrigo <chistiakov.dn@dom.ru>
2026-07-30 09:55:22 +0500

    add secret vars from env

--- TestBlog/settings.py
+++ TestBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d2qt4-2sa@q+4n_%zi4vuaw&n(daz-my=x=&9+urvk0i$#(v(g"
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(env_file=BASE_DIR / ".env")
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
