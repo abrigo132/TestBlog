62e13f109d4f4cb1509a3466af158552b2e89900
abrigo <chistiakov.dn@dom.ru>
2026-07-30 09:56:15 +0500

    add postgresql connection

--- TestBlog/settings.py
+++ TestBlog/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = "TestBlog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["CONN_MAX_AGE"] = int(env("CONN_MAX_AGE"))
 
 
 # Password validation
