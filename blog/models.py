dda44c72bc79b07f5ee1f21f0db59a7a5e13692a
abrigo <chistiakov.dn@dom.ru>
2026-07-30 13:18:02 +0500

    init blog django app

---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержание")
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name="Автор",
+    )
+    CATEGORY_CHOICES = [
+        ("traditional", "Традиционные инвестиции"),
+        ("crypto", "Крипто инвестиции"),
+    ]
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default="traditional",
+        verbose_name="Категория",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлен")
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.title
