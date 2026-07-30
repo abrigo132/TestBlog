4e9fefa304b5b8f209ca51e8c343c1784a186096
abrigo <chistiakov.dn@dom.ru>
2026-07-30 14:06:03 +0500

    import comment routers

--- blog/routers.py
+++ blog/routers.py
@@ -76,3 +76,90 @@ def delete_post(request, post_id: int):
     crud.delete_post(post)
 
     return 201, None
+
+
+@router.get("/{post_id}/comments", response=List[CommentResponse])
+def list_comments(request, post_id: int):
+    if not crud.get_post(post_id=post_id):
+        return 404, {"detail": "Пост не найден"}
+
+    comments = crud.get_comments_for_post(post_id=post_id)
+    return comments
+
+
+@router.post(
+    "/{post_id}/comments",
+    response={201: CommentResponse, 404: dict},
+    auth=JWTAuth(),
+)
+def create_comment_for_post(request, post_id: int, payload: CommentCreate):
+    post = crud.get_post(post_id=post_id)
+    if post is None:
+        return 404, {"detail": "Пост не найден"}
+
+    comment = crud.create_comment(
+        post=post,
+        content=payload.content,
+        user=request.auth,
+    )
+    return 201, comment
+
+
+@router.put(
+    "/{post_id}/comments/{comment_id}",
+    response={200: CommentResponse, 404: dict, 403: dict},
+    auth=JWTAuth(),
+)
+def update_comment_for_post(
+    request,
+    post_id: int,
+    comment_id: int,
+    comment_payload: CommentUpdate,
+):
+    post = crud.get_post(post_id=post_id)
+
+    if post is None:
+        return 404, {"detail": "Пост не найден"}
+
+    comment = crud.get_comment_by_id(comment_id=comment_id)
+
+    if comment is None:
+        return 404, {"detail": "Комментарий не найден"}
+
+    if comment.post_id != post_id:
+        return 404, {"detail": "Комментарий не найден в этом посте"}
+
+    if comment.author != request.auth:
+        return 403, {"detail": "Вы не можете изменять чужой комментарий"}
+
+    updated_comment = crud.update_comment(
+        comment=comment, content=comment_payload.content
+    )
+
+    return 200, updated_comment
+
+
+@router.delete(
+    "/{post_id}/comments/{comment_id}",
+    response={204: None, 404: dict, 403: dict},
+    auth=JWTAuth(),
+)
+def delete_comment_for_post(
+    request,
+    post_id: int,
+    comment_id: int,
+):
+    comment = crud.get_comment_by_id(comment_id=comment_id)
+
+    if comment is None:
+        return 404, {"detail": "Комментарий не найден"}
+
+    if comment.post_id != post_id:
+        return 404, {"detail": "Комментарий не найден в этом посте"}
+
+    if comment.author != request.auth:
+        return 403, {"detail": "Вы не можете удалять чужой комментарий"}
+
+    crud.delete_comment(comment=comment)
+
+    return 204, None
